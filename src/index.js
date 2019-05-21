3496541bb392f685880a5bf48b95deb4d0c047ce
Maciej Jałocha <maciej.jalocha.pl@gmail.com>
2019-05-21 22:35:58 +0200

    Changed Mail

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
 //by clicking sign up button in the header you will be moved to email section
 const scrollToRegister = () => {
-	let element = document.getElementById("footer");
-	element.scrollIntoView({
-		behavior: "smooth",
-		block: "start",
-		inline: "nearest"
-	});
+  let element = document.getElementById("footer");
+  element.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest"
+  });
 };
 document
-	.getElementById("register_anchor")
-	.addEventListener("click", scrollToRegister);
+  .getElementById("register_anchor")
+  .addEventListener("click", scrollToRegister);
 
 //basic protection for mail
 const sendMailNormal = () => {
-	let username = "maciej.jalocha.pl",
-		domain = "gmail.com";
-	window.location.href =
-		"mailto:" + username.substring(0) + "@" + domain.substring(0);
+  let username = "maciej.jalocha.pl",
+    domain = "gmail.com";
+  window.location.href =
+    "mailto:" + username.substring(0) + "@" + domain.substring(0);
 };
 
 document
-	.getElementById("fake_address")
-	.addEventListener("click", sendMailNormal);
-
-const sendMailBusiness = () => {
-	let username = "wayweapp",
-		domain = "gmail.com";
-	window.location.href =
-		"mailto:" + username.substring(0) + "@" + domain.substring(0);
-};
+  .getElementById("fake_address")
+  .addEventListener("click", sendMailNormal);
 
 document
-	.getElementById("business-mail")
-	.addEventListener("click", sendMailBusiness);
+  .getElementById("business-mail")
+  .addEventListener("click", sendMailNormal);
